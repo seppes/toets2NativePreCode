321437d8b2936a316be4d53e7af9e015be3a93da
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-03-07 13:56:01 +0100

    versie02

--- lib/quizParts/vraag.dart
+++ lib/quizParts/vraag.dart
@@ -7,6 +7,28 @@ import 'antwoordKnop.dart';
 class Vraag extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.red);
+    return ListView(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+                padding: EdgeInsets.only(left: lib.tekstMarge),
+                child: Text("vraag 1 van 5", style: lib.basisTekst,)
+            ),
+            Container(
+              padding: EdgeInsets.only(right: lib.tekstMarge),
+              child: Text("score: 10", style: lib.basisTekst,),
+            )
+          ],
+        ),
+        Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.25,
+            child: Image.asset('assets/images/flutter.png', fit: BoxFit.fitWidth,)
+        ),
+      ],
+    );
   }
 }
