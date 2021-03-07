cb69b9f5cf9eabc28d2cb4c22ab629c7bc377a33
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-03-07 20:37:52 +0100

    versie10 gecorrigeerd

--- lib/quizParts/antwoordKnop.dart
+++ lib/quizParts/antwoordKnop.dart
@@ -21,20 +21,20 @@ class AntwoordKnop extends StatefulWidget {
 
 class _AntwoordKnopState extends State<AntwoordKnop> with SingleTickerProviderStateMixin {
   AnimationController animatieController;
-  Animation<Color> kleurAnimatie, randAnimatie;
+  Animation<Color> boxAnimatie, borderAnimatie;
+  ColorTween boxTween = ColorTween(begin: Colors.blue[100]);
+  ColorTween borderTween = ColorTween(begin: Colors.blue[100]);
   Animation<double> vervaagAnimatie;
-  ColorTween kleurTween = ColorTween(begin: Colors.blue[100], end: Colors.red);
-  ColorTween randTween = ColorTween(begin: Colors.blue[100], end: Colors.red);
   final AudioCache audioSpeler = AudioCache();
 
   @override
   void initState() {
     animatieController = AnimationController(duration: Duration(milliseconds: 500), vsync: this);
-    kleurAnimatie = kleurTween.animate(animatieController);
-    randAnimatie = randTween.animate(animatieController);
+    boxAnimatie = boxTween.animate(animatieController);
+    borderAnimatie = borderTween.animate(animatieController);
     vervaagAnimatie = Tween<double>(begin: 1, end: 0.3).animate(animatieController);
-    kleurAnimatie.addListener(() { setState(() { }); });
-    kleurAnimatie.addStatusListener((status) {
+    boxAnimatie.addListener(() { setState(() { }); });
+    boxAnimatie.addStatusListener((status) {
       if (status==AnimationStatus.completed) {
         widget.verwerkAntwoord(widget.correct);
         animatieController.reset();
@@ -52,10 +52,6 @@ class _AntwoordKnopState extends State<AntwoordKnop> with SingleTickerProviderSt
   @override
   Widget build(BuildContext context) {
     double knopBreedte = MediaQuery.of(context).size.width * 0.5 - lib.tekstMarge * 2;
-    Color eindKleur = widget.correct ? Colors.green : Colors.red;
-    print(widget.antwoord + ' : ' + widget.correct.toString() + ' -> ' + eindKleur.toString());
-    kleurTween.end = eindKleur;
-    randTween.end = eindKleur;
 
     return Padding(
       padding: const EdgeInsets.all(lib.tekstMarge),
@@ -65,12 +61,12 @@ class _AntwoordKnopState extends State<AntwoordKnop> with SingleTickerProviderSt
               constraints: BoxConstraints(minHeight: knopBreedte * 0.5, minWidth: knopBreedte, maxWidth: knopBreedte),
               child: DecoratedBox(
                   decoration: BoxDecoration(
-                      color: kleurAnimatie.value,
+                      color: boxAnimatie.value,
                       borderRadius: BorderRadius.circular(12),
                       boxShadow: [
                         BoxShadow(color: Colors.black, blurRadius: 1.0)
                       ],
-                      border: Border.all(color: randAnimatie.value, width: 2)
+                      border: Border.all(color: borderAnimatie.value, width: 2)
                   ),
                   child: Center(
                      child: Text(widget.antwoord, textAlign: TextAlign.center, style: lib.basisTekst)
@@ -80,6 +76,7 @@ class _AntwoordKnopState extends State<AntwoordKnop> with SingleTickerProviderSt
           opacity: vervaagAnimatie.value,
         ),
         onTap: () {
+            boxTween.end = borderTween.end = widget.correct ? Colors.green : Colors.red;
             animatieController.forward();
             audioSpeler.play(widget.geluidsBestand);
           },
