8a33f4836a7e7a8e6d1cf645843e3d4fabb9229b
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-03-11 19:05:11 +0100

    afbeeldingen toegevoegd

--- lib/bibliotheek.dart
+++ lib/bibliotheek.dart
@@ -1,17 +1,12 @@
 library bibliotheek;
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 const double tekstMarge = 10.0;
-const TextStyle basisTekst = TextStyle(
-    fontFamily: 'Verdana',
-    fontSize: 16
-);
-const TextStyle kopTekst = TextStyle(
-    fontFamily: 'Verdana',
-    fontSize: 18,
-    fontWeight: FontWeight.bold
-);
+const TextStyle basisTekst = TextStyle(fontFamily: 'Verdana', fontSize: 16);
+const TextStyle kopTekst =
+    TextStyle(fontFamily: 'Verdana', fontSize: 18, fontWeight: FontWeight.bold);
 
 List<dynamic> vragen = [
   {
@@ -31,6 +26,135 @@ List<dynamic> vragen = [
     "antwoorden": ["class", "Container", "const", "import"],
     "correct": 1,
     "afbeelding": "widgettree.png"
+  }
+];
+
+List<dynamic> vragen2 = [
+  {
+    "vraag": "In welk jaar ontstond het besturingssysteem MS-DOS?",
+    "antwoorden": ["1981", "1975", "1984", "1978"],
+    "correct": 0,
+    "afbeelding":
+        "http://marcdc-tm.github.io/flutter-quiz/afbeeldingen/msdos.png",
+    "categorie": ["ICT", "history"]
+  },
+  {
+    "vraag": "In welk jaar werd de Sovjet-Unie officieel opgericht?",
+    "antwoorden": ["1917", "1919", "1922", "1925", "1931"],
+    "correct": 2,
+    "afbeelding":
+        "http://marcdc-tm.github.io/flutter-quiz/afbeeldingen/sovjetunion.png",
+    "categorie": ["history"]
+  },
+  {
+    "vraag":
+        "Hendrik VIII was van 1509 tot 1547 koning van Engeland. Hoeveel keer is hij gehuwd?",
+    "antwoorden": ["5", "6", "7", "8"],
+    "correct": 1,
+    "afbeelding":
+        "http://marcdc-tm.github.io/flutter-quiz/afbeeldingen/henry8.jpg",
+    "categorie": ["history"]
+  },
+  {
+    "vraag":
+        "De stad Parijs werd gedurende een korte periode in 1871 bestuurd door een revolutionaire regering. Hoe noemt men die regering en periode?",
+    "antwoorden": [
+      "De Parijse omwenteling",
+      "Paris en flammes",
+      "Commune van Parijs",
+      "De Parijse Bevrijding",
+      "Liberté et Egalité"
+    ],
+    "correct": 2,
+    "afbeelding":
+        "http://marcdc-tm.github.io/flutter-quiz/afbeeldingen/commune.jpg",
+    "categorie": ["history"]
+  },
+  {
+    "vraag": "Waarvan is dit het logo?",
+    "antwoorden": ["Android Studio", "Dart", "Flutter", "Xcode", "IntelliJ"],
+    "correct": 2,
+    "afbeelding":
+        "http://marcdc-tm.github.io/flutter-quiz/afbeeldingen/flutterbis.png",
+    "categorie": ["ICT"]
+  },
+  {
+    "vraag": "Welke taal gebruiken we in Flutter?",
+    "antwoorden": ["C", "Dart", "Java", "Python"],
+    "correct": 1,
+    "afbeelding":
+        "http://marcdc-tm.github.io/flutter-quiz/afbeeldingen/programminglanguages.png",
+    "categorie": ["ICT"]
+  },
+  {
+    "vraag": "Wat is een voorbeeld van een widget?",
+    "antwoorden": ["class", "Container", "const", "import"],
+    "correct": 1,
+    "afbeelding":
+        "http://marcdc-tm.github.io/flutter-quiz/afbeeldingen/widgettree.png",
+    "categorie": ["ICT"]
+  },
+  {
+    "vraag":
+        "In welk jaar werd de Amerikaanse onafhankelijkheidsverklaring ondertekend?",
+    "antwoorden": ["1821", "1813", "1776", "1795"],
+    "correct": 2,
+    "afbeelding":
+        "http://marcdc-tm.github.io/flutter-quiz/afbeeldingen/usindependence.jpg",
+    "categorie": ["history"]
+  },
+  {
+    "vraag": "Wie creëerde rons 1972 de programmeertaal Prolog?",
+    "antwoorden": [
+      "Robert Kowalski",
+      "Alain Colmerauer",
+      "William Carlsson",
+      "Paul Allen"
+    ],
+    "correct": 1,
+    "afbeelding":
+        "http://marcdc-tm.github.io/flutter-quiz/afbeeldingen/alainc.jpg",
+    "categorie": ["ICT", "history"]
+  },
+  {
+    "vraag":
+        "In 1953 ontwikkelde John W. Backus een praktischer alternatief voor assembleertaal. Welke programmeertaal is toen ontstaan?",
+    "antwoorden": ["COBOL", "Java", "Basic", "LISP", "Fortran"],
+    "correct": 4,
+    "afbeelding":
+        "http://marcdc-tm.github.io/flutter-quiz/afbeeldingen/programl.jpg",
+    "categorie": ["ICT", "history"]
+  },
+  {
+    "vraag":
+        "Aan hoeveel mensen heeft naar schatting de tweede wereldoorlog het leven gekost?",
+    "antwoorden": ["18 miljoen", "103 miljoen", "55 miljoen", "39 miljoen"],
+    "correct": 2,
+    "afbeelding":
+        "http://marcdc-tm.github.io/flutter-quiz/afbeeldingen/wo2doden.jpg",
+    "categorie": ["history"]
+  },
+  {
+    "vraag": "Welke widget heeft maar 1 child?",
+    "antwoorden": ["Column", "Container", "ListView", "Row"],
+    "correct": 1,
+    "afbeelding":
+        "http://marcdc-tm.github.io/flutter-quiz/afbeeldingen/widgetchild.png",
+    "categorie": ["ICT"]
+  },
+  {
+    "vraag":
+        "In welk bestand declareert u afbeeldingen en andere bronnen in Flutter?",
+    "antwoorden": [
+      "assets",
+      "main.dart",
+      "pubspec.\nyaml",
+      "Runner.\nxcodeproj"
+    ],
+    "correct": 2,
+    "afbeelding":
+        "http://marcdc-tm.github.io/flutter-quiz/afbeeldingen/pubspec.png",
+    "categorie": ["ICT"]
   }
 ];
 
